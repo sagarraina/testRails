7c48524d3e6686beccd1cb2ddf314f15b7ea1ae3
sagarraina <sags007@gmail.com>
2015-10-06 10:20:59 -0400

    This is sagars 2 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-def testing_rails
+def testing_ruby
 end
 end
